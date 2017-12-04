386d8bc89499b76ffa456ea646cb7a998ed9eb06
moscibrodzka <m.moscibrodzka@astro.ru.nl>
2017-12-04 17:19:25 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-#usage: mibothros theta freq filename Munit trat_j trat_d
+#usage: ./ipole theta freq filename Munit trat_j trat_d
 #
 #
 ./ipole 20. 230e9 HARM3D.001500.h5 1e19 3. 3.
+
