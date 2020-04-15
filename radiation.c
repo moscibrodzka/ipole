f018761cdbc2cc1f502b6fc55fc0e97926c7b827
Monika Moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-04-15 16:21:59 +0200

    additional function for bk angle for NT model

--- radiation.c
+++ radiation.c
@@ -1,8 +1,6 @@
-
 /* 
 
-model-independent
-radiation-related utilities.
+model-independent radiation-related utilities.
 
 */
 
@@ -15,10 +13,10 @@ double Bnu_inv(double nu, double Thetae)
     x = HPL * nu / (ME * CL * CL * Thetae);
 
     if (x < 2.e-3)		/* Taylor expand */
-	return ((2. * HPL / (CL * CL)) /
-		(x / 24. * (24. + x * (12. + x * (4. + x)))));
+      return ((2. * HPL / (CL * CL)) /
+    	      (x / 24. * (24. + x * (12. + x * (4. + x)))));
     else
-	return ((2. * HPL / (CL * CL)) / (exp(x) - 1.));
+      return ((2. * HPL / (CL * CL)) / (exp(x) - 1.));
 }
 
 /* get jnu, and convert to jnu/nu^2 = jnu_invariant */
@@ -62,6 +60,33 @@ double get_bk_angle(double X[NDIM], double Kcon[NDIM], double Ucov[NDIM])
 
     get_model_bcov(X, Bcov);
     get_model_bcon(X, Bcon);
+
+    B = sqrt(fabs
+	     (Bcon[0] * Bcov[0] + Bcon[1] * Bcov[1] + Bcon[2] * Bcov[2] +
+	      Bcon[3] * Bcov[3]));
+
+    if (B == 0.)
+	return (M_PI / 2.);
+
+    k = fabs(Kcon[0] * Ucov[0] + Kcon[1] * Ucov[1] + Kcon[2] * Ucov[2] +
+	     Kcon[3] * Ucov[3]);
+
+    mu = (Kcon[0] * Bcov[0] + Kcon[1] * Bcov[1] + Kcon[2] * Bcov[2] +
+	  Kcon[3] * Bcov[3]) / (k * B);
+
+    if (fabs(mu) > 1.)
+	mu /= fabs(mu);
+
+    if (isnan(mu))
+	fprintf(stderr, "isnan get_bk_angle\n");
+
+    return (acos(mu));
+}
+
+/*here Bcon and Bcov are parameters, used in NT model*/
+double get_bk_angle2(double X[NDIM], double Kcon[NDIM], double Ucov[NDIM],double Bcon[NDIM],double Bcov[NDIM])
+{
+    double B, k, mu;
 
     B = sqrt(fabs
 	     (Bcon[0] * Bcov[0] + Bcon[1] * Bcov[1] + Bcon[2] * Bcov[2] +
