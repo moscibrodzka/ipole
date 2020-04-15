33fd740f43de75205772eb2b6dd6cf3712302e0c
Monika Moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-04-15 16:04:28 +0200

    no changes

--- geodesics.c
+++ geodesics.c
@@ -7,21 +7,21 @@ this is the main photon orbit integrator
 
 */
 
-
 void push_photon(double X[NDIM], double Kcon[NDIM], double dl,double Xhalf[NDIM],double Kconhalf[NDIM])
 {
-	double lconn[NDIM][NDIM][NDIM];
+  	double lconn[NDIM][NDIM][NDIM];
 	double dKcon[NDIM];
 	double Xh[NDIM], Kconh[NDIM];
 	int i, j, k;
 
+	
 	/* 2nd order: scheme
 	   take half-step and then evaluate derivatives 
 	   at half-step. */
 
 	/** half-step **/
 	get_connection(X, lconn) ;
-
+	
 	/* advance K */
 	for (k = 0; k < 4; k++)
 		dKcon[k] = 0.;
@@ -43,11 +43,9 @@ void push_photon(double X[NDIM], double Kcon[NDIM], double dl,double Xhalf[NDIM]
 	for (i = 0; i < 4; i++) Kconhalf[i]=Kconh[i];
 	/********************/
 
-
 	/** full step **/
 	get_connection(Xh, lconn) ;
-
-
+	
 	/* advance K */
 	for (k = 0; k < 4; k++)
 		dKcon[k] = 0.;
@@ -65,12 +63,15 @@ void push_photon(double X[NDIM], double Kcon[NDIM], double dl,double Xhalf[NDIM]
 		X[k] += dl * Kconh[k];
 
 	/*
+	double Kcov[NDIM];
+	double gcov[NDIM][NDIM];
 	gcov_func(X,gcov);
 	lower(Kcon, gcov, Kcov);
-	E0 = -Kcov[0];
-	L0 = Kcov[3];
+	double E0 = -Kcov[0];
+	double L0 = Kcov[3];
 	fprintf(stdout,"along g: r=%g E0=%g L0=%g \n",exp(X[1]),E0,L0);
 	*/
 	/* done! */
 }
+
 
