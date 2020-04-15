08843c85cfe6329c5b3dd15a63e1156d79832fb1
Monika Moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-04-15 16:49:19 +0200

    additional parameter for fmks system

--- defs.h
+++ defs.h
@@ -1,20 +1,26 @@
-
-
 /* some coordinate parameters */
 double a;
 double R0 ;
 double Rin ;
 double Rout ;
-double Rh ;
+double Rh,Risco ;
 double hslope ;
 double th_len, th_beg;
 double startx[NDIM], stopx[NDIM], dx[NDIM];
-double gam ;
+double gam,game,gamp ;
+double DTd;
+double t0;
+
+double sigma_cut;
+
+//additional for fmks
+double mks_smooth,poly_alpha,poly_norm,poly_xt;
 
 /* electron temperature model parameters */
 double trat_j;
 double theta_j;
 double trat_d;
+double rmax;
 
 /* HARM model globals */
 double M_unit;
@@ -26,5 +32,15 @@ double B_unit;
 
 int N1, N2, N3;
 
+double ****bcon;
+double ****bcov;
+double ****ucon;
+double ****ucov;
+double ****p;
+double ***ne;
+double ***uu;
+double ***thetae;
+double ***b;
+double ***sigma_m;
 
 
