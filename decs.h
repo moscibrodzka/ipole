6456173f96bf14ddbf212663a8ab5f7e4405d801
Monika Moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-04-15 16:48:35 +0200

    added new params

--- decs.h
+++ decs.h
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -15,11 +14,31 @@
 #include <omp.h>
 #endif
 
-#define NX   128
-#define NY   128
+//parameters to change
+#define FMKS 0 //works for eht runs
+#define MKS  1 //works for my old runs as well
+#define THERMAL 1
+#define POWERL  0
+#define KAPPAL  0
+#define NX  160
+#define NY  160
+#define THETAE_MIN 1e-3
+#define THETAE_MAX 1000.
+#define NPRIM	10 // 10 variables in hdf5 in fmks
+//#define NPRIM	8 //
 
-#define NDIM	4
-#define NPRIM	8
+//chose integration scheme
+#define INT_FULL  1 //full integration step
+#define INT_SPLIT 0 //split: 1/2rot + ea + 1/2rot (from development version of ipole)
+
+//colortheme for ppm files
+#define RAINBOW 1
+#define AFMHOT 0
+#define BW 0
+
+#define STRLEN (2048)
+#define NIMG (4) //just stokes here
+#define NDIM  4
 
 /* mnemonics for primitive vars; conserved vars */
 #define KRHO    0
@@ -31,12 +50,16 @@
 #define B2      6
 #define B3      7
 
+#define KEL      8
+#define KTOT     9
+
 /* numerical convenience */
 #define SMALL	1.e-40
-
+#define SMALLER	1.e-50
+					     
 #define sign(x) (((x) < 0) ? -1 : ((x) > 0))
-#define MAX(a,b) (((a)>(b))?(a):(b))
-#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(c,b) (((c)>(b))?(c):(b))
+#define MIN(c,b) (((c)<(b))?(c):(b))
 
 /* some coordinate parameters */
 extern double a;
@@ -44,16 +67,24 @@ extern double a;
 extern double theta_j;
 extern double trat_j;
 extern double trat_d;
-//2d
+extern double rmax;
 
+//2d
 extern double R0 ;
 extern double Rin ;
 extern double Rout ;
-extern double Rh ;
+extern double Rh,Risco ;
 extern double hslope ;
 extern double th_len,th_beg;
 extern double startx[NDIM], stopx[NDIM], dx[NDIM];
-extern double gam ;
+extern double gam,game,gamp ;
+extern double DTd;
+extern double t0;
+
+extern double sigma_cut;
+
+//additional for fmks
+extern double mks_smooth,poly_alpha,poly_norm,poly_xt;
 
 /* HARM model globals */
 extern double M_unit;
@@ -65,55 +96,72 @@ extern double B_unit;
 
 extern int N1, N2, N3;
 
+extern double ****bcon;
+extern double ****bcov;
+extern double ****ucon;
+extern double ****ucov;
+extern double ****p;
+extern double ***ne;
+extern double ***uu;
+extern double ***thetae;
+extern double ***b;
+
+
+
+
+double rhorizon_calc(int pos_sign);
+double risco_calc( int do_prograde );
 
 /** model-independent subroutines **/
-/* core routines */
+/* core routines in main.c */
 void init_XK(int i, int j, double Xcam[4], double fovx, double fovy, double X[4], double Kcon[4]) ;
 void null_normalize(double Kcon[NDIM], double fnorm) ;
 void normalize(double *vcon, double gcov[][NDIM]);
 double approximate_solve(double Ii, double ji, double ki, double jf, double kf, double dl) ;
-void get_jkinv(double X[NDIM], double Kcon[NDIM], double *jnuinv, double *knuinv);
-int    stop_forward_integration(double X[NDIM], double Kcon[NDIM],
-	double Xcam[NDIM]) ;
-int    stop_backward_integration(double X[NDIM], double Kcon[NDIM],
-	double Xcam[NDIM]) ;
-void dump(double image[NX][NY],double imageS[NX][NY][NDIM], char *fname, double scale) ;
+void get_jkinv(double X[NDIM], double Kcon[NDIM], double *jnuinv, double *knuinv,double col_v[NDIM]);
+int    stop_backward_integration(double X[NDIM], double Kcon[NDIM],double Xcam[NDIM]) ;
+void dump(double image[NX][NY],double imageS[NX][NY][NDIM], double col_var[NX][NY][3],char *fname, double scale) ;
 
 /* geodesic integration */
-//void   push_photon(double X[NDIM], double Kcon[NDIM], double dl);
-void   push_photon(double X[NDIM], double Kcon[NDIM], double dl, double Xhalf[NDIM],double Kconhalf[NDIM]);
-void   push_photon_rk4(double X[NDIM], double Kcon[NDIM], double dl);
-void   push_photon_gsl(double X[NDIM], double Kcon[NDIM], double dl);
+void push_photon(double X[NDIM], double Kcon[NDIM], double dl, double Xhalf[NDIM],double Kconhalf[NDIM]);
 
 /* model */
 void   gcov_func(double *X, double gcov[][NDIM]);
 void   gcov_func_rec(double *X, double gcov[][NDIM]);
-void   gcon_func(double gcov[][NDIM], double gcon[][NDIM]);
+int   gcon_func(double gcov[][NDIM], double gcon[][NDIM]);
 double gdet_func(double gcov[][NDIM]);
+
+void   set_dxdX(double *X, double dxdX[NDIM][NDIM]);
 void   get_connection(double *X, double lconn[][NDIM][NDIM]);
 void   get_connection_num(double *X, double lconn[][NDIM][NDIM]);
+
 void   lower(double *ucon, double Gcov[NDIM][NDIM], double *ucov);
 void   raise(double *ucov, double Gcon[NDIM][NDIM], double *ucon);
 double stepsize(double X[NDIM], double K[NDIM]);
+
 void init_model(char *args[]) ;
 double get_model_thetae(double X[NDIM]) ;
 double get_model_b(double X[NDIM]) ;
 double get_model_ne(double X[NDIM]) ;
+double get_model_uu(double X[NDIM]) ;
 void get_model_bcov(double X[NDIM], double Bcov[NDIM]) ;
 void get_model_bcon(double X[NDIM], double Bcon[NDIM]) ;
 void get_model_ucov(double X[NDIM], double Ucov[NDIM]) ;
 void get_model_ucon(double X[NDIM], double Ucon[NDIM]) ;
-
+void update_data();
 /* harm utilities */
 /*
 void interp_fourv(double X[NDIM], double ***fourv, double Fourv[NDIM]) ;
 double interp_scalar(double X[NDIM], double **var) ;
 void Xtoij(double X[NDIM], int *i, int *j, double del[NDIM]) ;
 */
-void   bl_coord(double *X, double *r, double *th);
+void  bl_coord(double *X, double *r, double *th);
 //void coord(int i, int j, double *X) ;
 void set_units(char *instr) ;
+//for fast light
 void init_physical_quantities(void) ;
+//for slow light
+//void init_physical_quantities(int n) ;
 
 //for 3d coment out
 /*
@@ -122,7 +170,6 @@ void **malloc_rank2(int n1, int n2, int size) ;
 void ***malloc_rank3(int n1, int n2, int n3, int size) ;
 void ****malloc_rank4(int n1, int n2, int n3, int n4, int size) ;
 */
-
 void init_storage(void) ;
 
 /* tetrad related */
@@ -136,24 +183,32 @@ void   make_plasma_tetrad(double Ucon[NDIM], double Kcon[NDIM], double Bcon[NDIM
 /* imaging */
 void make_ppm(double p[NX][NY], double freq, char filename[]) ;
 void rainbow_palette(double data, double min, double max, int *pRed, int *pGreen, int *pBlue) ;
+void monika_palette(double data, double min, double max, int *pRed, int *pGreen, int *pBlue) ;
 
 /* radiation */
 double Bnu_inv(double nu, double Thetae) ;
 double jnu_inv(double nu, double Thetae, double Ne, double B, double theta) ;
 double get_fluid_nu(double Kcon[NDIM], double Ucov[NDIM]) ;
 double get_bk_angle(double X[NDIM], double Kcon[NDIM], double Ucov[NDIM]) ;
+double get_bk_angle2(double X[NDIM], double Kcon[NDIM], double Ucov[NDIM], double Bcon[NDIM],double Bcov[NDIM]) ;
 
 /* emissivity */ 
 double jnu_synch(double nu, double Ne, double Thetae, double B, double theta) ;
+double jnu_synchSI(double nu, double Ne, double Thetae, double B,
+		   double theta);
+double jnu_synchSQ(double nu, double Ne, double Thetae, double B,
+		   double theta);
+double jnu_synchSV(double nu, double Ne, double Thetae, double B,
+		   double theta);
 
-#define DLOOP  for(k=0;k<NDIM;k++)for(l=0;l<NDIM;l++)
 
 void init_N(double Xi[NDIM],double Kconi[NDIM],double complex Ncon[NDIM][NDIM]);
 void evolve_N(double Xi[NDIM],double Kconi[NDIM],
 	      double Xf[NDIM],double Kconf[NDIM],
 	      double Xhalf[NDIM],double Kconhalf[NDIM],
 	      double dlam,
-	      double complex N_coord[NDIM][NDIM]);
+	      double complex N_coord[NDIM][NDIM],
+	      double *tauF);
 void project_N(double X[NDIM],double Kcon[NDIM],
 	double complex Ncon[NDIM][NDIM],
 	double *Stokes_I, double *Stokes_Q,double *Stokes_U,double *Stokes_V);
