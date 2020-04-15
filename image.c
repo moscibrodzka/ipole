244c99140b7cef82fa223b6dd6e44d65d30e5764
Monika Moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-04-15 16:11:56 +0200

    new color schemes

--- image.c
+++ image.c
@@ -60,11 +60,18 @@ void make_ppm(double p[NX][NY], double freq, char filename[])
 	for (j = NY-1; j >= 0; j--) 
 	for (i = 0; i < NX; i++) 
 	{
-	  rainbow_palette(p[i][j],min,max,&red,&green,&blue) ;
-	  //monika_palette(p[i][j],min,max,&red,&green,&blue) ;
-	  fputc((char) red, fp);
-	  fputc((char) green, fp);
-	  fputc((char) blue, fp);
+	    if(RAINBOW){
+		rainbow_palette(p[i][j],min,max,&red,&green,&blue) ;
+	    }
+	    if(BW){
+		monika_palette(p[i][j],min,max,&red,&green,&blue) ;
+	    }
+	    if(AFMHOT){
+		afmhot_palette(p[i][j],min,max,&red,&green,&blue) ;
+	    }
+	    fputc((char) red, fp);
+	    fputc((char) green, fp);
+	    fputc((char) blue, fp);
 	}
 
 	fclose(fp);
@@ -131,7 +138,7 @@ void rainbow_palette(double data, double min, double max, int *pRed, int *pGreen
 
 
 
-
+//black & white
 void monika_palette(double data, double min, double max, int *pRed, int *pGreen, int *pBlue)
 {
   double aa, b, c, d, e, f;
@@ -158,7 +165,7 @@ void monika_palette(double data, double min, double max, int *pRed, int *pGreen,
     e = 3.52941176470581 - 4.0*x;
     f = d < e ? d : e;
     y = c > f ? c : f;
-    y=x;
+    y = x;
     *pGreen = (int)(255.0*y);
     *pGreen = *pGreen >   0 ? *pGreen :   0;
     *pGreen = *pGreen < 255 ? *pGreen : 255;
@@ -167,7 +174,57 @@ void monika_palette(double data, double min, double max, int *pRed, int *pGreen,
     aa = 4.0*x + 0.498039215686276;
     b = 2.50980392156862 - 4.0*x;
     y = aa < b ? aa : b;
-    y=x;
+    y = x;
+    *pBlue = (int)(255.0*y);
+    *pBlue = *pBlue >   0 ? *pBlue :   0;
+    *pBlue = *pBlue < 255 ? *pBlue : 255;
+
+  }
+  else {
+    *pRed = *pGreen = *pBlue = (data > max ? 255: 0) ;
+  }
+
+  return;
+}
+
+// AFMHOT
+void afmhot_palette(double data, double min, double max, int *pRed, int *pGreen, int *pBlue)
+{
+  double aa, b, c, d, e, f;
+  double x, y;
+//  max=max*0.1;
+  double max_min = max - min ;
+
+  if(max_min > 0.0) { // trust no one
+    x = (data - min)/(max_min) ;
+
+    /* ========== Red ============ */
+     aa = 4.0*x - 1.52549019607844;
+     b = 4.52941176470589 - 4.0*x;
+     y = aa < b ? aa : b;
+     y = 2*x;
+     *pRed = (int)(255.0*y);
+     *pRed = *pRed >   0 ? *pRed :   0;
+     *pRed = *pRed < 255 ? *pRed : 255;
+     
+    /* ========== Green ========== */
+    aa = 4.0*x - 0.521568627450979;
+    b = 2.52549019607844 - 4.0*x;
+    c = aa < b ? aa : b;
+    d = 4.0*x - 1.53725490196073;
+    e = 3.52941176470581 - 4.0*x;
+    f = d < e ? d : e;
+    y = c > f ? c : f;
+    y = 2*x-0.5;
+    *pGreen = (int)(255.0*y);
+    *pGreen = *pGreen >   0 ? *pGreen :   0;
+    *pGreen = *pGreen < 255 ? *pGreen : 255;
+
+    /* ========== Blue =========== */
+    aa = 4.0*x + 0.498039215686276;
+    b = 2.50980392156862 - 4.0*x;
+    y = aa < b ? aa : b;
+    y = 2*x-1;
     *pBlue = (int)(255.0*y);
     *pBlue = *pBlue >   0 ? *pBlue :   0;
     *pBlue = *pBlue < 255 ? *pBlue : 255;
