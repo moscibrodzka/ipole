db1db0c5c4ffc89d364e9cd76dca7ebb9ce8d03d
moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-04-16 14:23:49 +0200

    follows IAU conventions

--- ipole.py
+++ ipole.py
@@ -4,32 +4,29 @@ import numpy as np
 import sys
 
 if len(sys.argv) < 2 :
-	print "usage: ipole.py ipole.dat"
+	print ("usage: python3 ipole.py ipole.dat")
 	quit()
 
 fil = sys.argv[1]
 
 # read in data 
-i0, j0, Ia, Is, Qs, Us, Vs = np.loadtxt(fil, unpack=True)
+i0, j0, Ia, Is, Qs, Us, Vs, tauF = np.loadtxt(fil, unpack=True)
 
 # set image size
 ImRes = int(round(np.sqrt(len(i0))))
-print "Image resolution: ", ImRes
+print ("Image resolution: ", ImRes)
 
 # size of single pixel in rad: M/pixel . muas/pix . rad/muas
-FOV = 40.
-print "assuming FOV = ", FOV, "GM/c^2"
+FOV = 40#4.17
+print ("assuming FOV = ", FOV, "GM/c^2")
 #
-da = FOV/ImRes * 5. / (1.e6 * 206265.)
 # solid angle subtended by pixel
-dO = da*da
-Jy = 1.e-23  # cgs
-flux = dO*sum(Is)/Jy
-print "Flux [Jy]: ", flux
+flux = sum(Is)
+print ("Flux [Jy]: ", flux)
 
 # recast indices into offset in units of M
-i = (np.reshape(i0, (ImRes,ImRes))+1)*40./ImRes - 20.
-j = (np.reshape(j0, (ImRes,ImRes))+1)*40./ImRes - 20.
+i = (np.reshape(i0, (ImRes,ImRes))+1)*FOV/ImRes - FOV/2.
+j = (np.reshape(j0, (ImRes,ImRes))+1)*FOV/ImRes - FOV/2.
 
 # LP plot
 plt.subplot(2,2,2)
@@ -37,16 +34,17 @@ lpfrac = 100.*np.sqrt(Qs*Qs + Us*Us)/Is
 z = np.reshape(lpfrac, (ImRes,ImRes))
 plt.pcolormesh(i,j,z,cmap='jet', vmin = 0., vmax = 100.)
 plt.title('LP [%]')
-plt.axis([-20,20,-20,20])
+plt.axis([-FOV/2,FOV/2,-FOV/2,FOV/2])
 plt.colorbar()
 
 # EVPA plot
 plt.subplot(2,2,3)
-evpa = (180./3.14159)*0.5*np.arctan2(Us,Qs)
+# adjusted to IAU conventions
+evpa = np.angle(Qs+1j*Us)/2*180/np.pi
 z = np.reshape(evpa, (ImRes,ImRes))
 plt.pcolormesh(i,j,z,cmap='jet')
 plt.title('EVPA [deg]')
-plt.axis([-20,20,-20,20])
+plt.axis([-FOV/2,FOV/2,-FOV/2,FOV/2])
 plt.colorbar()
 
 # CP plot
@@ -55,31 +53,38 @@ cpfrac = 100.*Vs/Is
 z = np.reshape(cpfrac, (ImRes,ImRes))
 plt.pcolormesh(i,j,z,cmap='jet', vmin = -5, vmax = 5.)
 plt.title('CP [%]')
-plt.axis([-20,20,-20,20])
+plt.axis([-FOV/2,FOV/2,-FOV/2,FOV/2])
 plt.colorbar()
 
 # total intensity 
 plt.subplot(2,2,1)
 z = np.reshape(Is, (ImRes,ImRes))
-plt.pcolormesh(i,j,z,cmap='afmhot', vmin=0., vmax=1.e-4)
+#plt.pcolormesh(i,j,z,cmap='afmhot', vmin=0., vmax=1.e-4)
+plt.pcolormesh(i,j,z,cmap='afmhot', vmin=0., vmax=5.e-4)
 plt.colorbar()
 plt.title('Stokes I [cgs]')
-plt.axis([-20,20,-20,20])
+plt.axis([-FOV/2,FOV/2,-FOV/2,FOV/2])
 
 # superpose EV on total intensity using quiver
 Qb = sum(Qs)
 Ub = sum(Us)
 LP = np.sqrt(Qb*Qb + Ub*Ub)/sum(Is)
-print "LP [%]: ", LP*100.
+print ("LP [%]: ", LP*100.)
 CHI = (180./3.14159)*0.5*np.arctan2(Ub,Qb)
-print "EVPA [deg]:",CHI
+print ("EVPA [deg]:",CHI)
 CP = sum(Vs)/sum(Is)
-print "CP [%]: ", CP*100.
+print ("CP [%]: ", CP*100.)
 amp = np.sqrt(Qs*Qs + Us*Us)
 scal = max(amp)
-#print(scal)
-vxp = np.sqrt(Qs*Qs + Us*Us)*np.cos(evpa*3.14159/180.)/scal
-vyp = np.sqrt(Qs*Qs + Us*Us)*np.sin(evpa*3.14159/180.)/scal
+
+
+# brand new adjusted to IAU conventions and ehtim
+vxp = -np.sqrt(Qs*Qs + Us*Us)*np.sin(evpa*3.14159/180.)/scal
+vyp = np.sqrt(Qs*Qs + Us*Us)*np.cos(evpa*3.14159/180.)/scal
+
+
+
+
 vx = np.reshape(vxp, (ImRes,ImRes))
 vy = np.reshape(vyp, (ImRes,ImRes))
 skip = 4
