0120a1a546453867e08e7a920b56fcb9c83521e2
moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-04-16 14:28:29 +0200

    configured to run model_harm3d

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 #
-#usage: ./ipole theta freq filename Munit trat_j trat_d sigma_cut
-#
+# to run different models makefile needs to substitute model_harm3d.c to your desired model
 # for model_harm3d.c model_harm2d.c model_iharm3d.c etc
+#usage: ./ipole theta freq filename Munit trat_j trat_d sigma_cut
 ./ipole 20. 230e9 HARM3D.001500.h5 1e19 3. 3. 2.
-
-# iharm3d.c
-#./ipole 17. 230e9 ../polarized_code_comparizon/mhdmodels/dump_00001096_MADa-0.5.h5 2.87962e+25 1. 160. 1.
-
 #
-# Novikov-Thorne disk test freq=1keV
+# Novikov-Thorne disk test 
 # usage: ./ipole theta freq BHspin
+# example
 #./ipole 75. 2.41e17 0.99
 
